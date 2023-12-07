430281ecd963b061b093f3a2f23db8c6332bf4de
Ozeliurs <ozeliurs@gmail.com>
2023-12-07 19:14:19 +0100

    feat: remove boilerplate, add basic api and homepage

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,58 @@
+import {Fragment} from 'react'
+import Link from "next/link";
+
+interface Page {
+    name: string
+    url: string
+    enabled: boolean
+}
+
+export default function NavBar() {
+
+    let title = "🌍 CrowdEnv"
+
+    let pages: Page[] = [
+        {
+            name: 'Quizz',
+            url: '/quizz',
+            enabled: false
+        },
+        {
+            name: 'CrowdData',
+            url: '/data',
+            enabled: false
+        },
+        {
+            name: 'Settings',
+            url: '/settings',
+            enabled: false
+        },
+    ]
+
+
+    return (
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <Link className="btn btn-ghost text-xl" href="/">{title}</Link>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal px-1">
+                    {
+                        pages.map((page: Page) => <li key={page.url}><Link href={page.url}>{page.name}</Link></li>)
+                    }
+                </ul>
+            </div>
+            <div className="dropdown dropdown-end">
+                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                    <div className="w-10 rounded-full">
+                        <img alt="Tailwind CSS Navbar component" src="http://thispersondoesnotexist.com/"/>
+                    </div>
+                </label>
+                <ul tabIndex={0}
+                    className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <li><a>Logout</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
