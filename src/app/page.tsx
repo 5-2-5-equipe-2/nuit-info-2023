115dc03842304c75ca2d978ffa3f75d668cb9e7e
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-12-08 06:05:26 +0100

    fix: missing dynamicalnksoikdmlq weaw

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import prisma from "@/lib/prisma";
 
+
+export const dynamic = 'force-dynamic';
 export default async function Home() {
     const deviceCount = await prisma.device.count()
     const metricCount = await prisma.metric.count()
