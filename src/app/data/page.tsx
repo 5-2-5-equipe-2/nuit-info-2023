a014026c29c7c2155d680d88072ff5054ca05986
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-12-08 05:33:08 +0100

    fix: missing dynamicalnksoikdmlq we

--- src/app/data/page.tsx
+++ src/app/data/page.tsx
@@ -4,6 +4,8 @@ import MetricTable from "@/app/components/table/MetricTable";
 import prisma from "@/lib/prisma";
 import DeviceTable from "@/app/components/table/DeviceTable";
 
+
+export const dynamic = 'force-dynamic';
 export default async function Page() {
     const metrics = await prisma.metric.findMany({})
     const devices = await prisma.device.findMany({})
